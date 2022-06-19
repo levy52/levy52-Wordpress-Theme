5348f313f6dba5239dbbd3682ed49bb968900b13
levy52 <levy52@vp.pl>
2022-06-19 21:25:52 +0200

    add comments.php

--- comments.php
+++ comments.php
@@ -38,14 +38,14 @@ if ( post_password_required() ) {
 
 	$comments_args = array(
         // change the title of send button 
-        'label_submit'=>'Send',
+        'label_submit'=> __( 'Send', 'levy52' ),
         // change the title of the reply section
-        'title_reply'=>'Write a Reply or Comment',
+        'title_reply'=>__( 'Write a Reply or Comment', 'levy52' ),
         // remove "Text or HTML to be displayed after the set of comment fields"
         'comment_notes_after' => '',
         // redefine your own textarea (the comment body)
-        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
-);
+        'comment_field' => '<span class="comment-form-comment"><label for="comment">' . __( 'Comment', 'levy52' ) . '</label><textarea id="comment" name="comment" aria-required="true"></textarea></span>',
+); 
 
 comment_form($comments_args);
 	?>
