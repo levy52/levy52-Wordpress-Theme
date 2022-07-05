36a4dd347b1cf7017af90dedf73145f30d4e6df5
levy52 <levy52@vp.pl>
2022-07-05 20:22:44 +0200

    add default image as there is no distinguishing image

--- template-parts/custom-loop.php
+++ template-parts/custom-loop.php
@@ -11,7 +11,7 @@ $blog_posts = new WP_Query(array('post_type' => 'post', 'post_status’' => 'pub
                     <div class="post_article_img"><a href="<?php the_permalink(); ?>"><?php if (has_post_thumbnail()) {
                                                         echo the_post_thumbnail('thumbnail', array('class' => 'post-cover'));
                                                     } else {
-                                                        echo '<img src="' . get_template_directory_uri() . '/assets/images/stop.jpg" width="100%" height="100%" style="
+                                                        echo '<img src="' . get_template_directory_uri() . '/assets/images/default-image.jpg" width="100%" height="100%" style="
                                                     aspect-ratio: 16/9;"/>';
                                                     } ?>
                     </div>
