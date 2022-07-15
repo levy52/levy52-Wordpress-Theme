2dfb07c5715efabdbacb7497c9c8835b2ddb0093
levy52 <radeklewicki52@gmail.com>
2022-07-15 20:26:49 +0200

    post image size changed

--- single.php
+++ single.php
@@ -6,7 +6,7 @@ get_header();
         <div class="col-12 col-md-8 col-xxl-9">
             <?php while (have_posts()) : the_post(); ?>
                 <h1 class="post_article_title fs-3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                <div class="post_article_img"><?php echo the_post_thumbnail('thumbnail', array('class' => 'post-cover')); ?> </div>
+                <div class="post_article_img"><?php echo the_post_thumbnail('full', array('class' => 'post-cover')); ?> </div>
                 <div class="text_post"><?php the_content(); ?></div>
                 <div class="col-12 mt-4 mb-3">
                     <?php wpb_posts_nav(); ?>
