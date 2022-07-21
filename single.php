1610a7645d9003d6f8e168876f6329c6ca652cb1
levy52 <radeklewicki52@gmail.com>
2022-07-21 20:30:46 +0200

    add social share buttons in article

--- single.php
+++ single.php
@@ -8,7 +8,16 @@ get_header();
                 <h1 class="post_article_title fs-3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
                 <div class="post_article_img"><?php echo the_post_thumbnail('full', array('class' => 'post-cover')); ?> </div>
                 <div class="text_post"><?php the_content(); ?></div>
-                <div class="col-12 mt-4 mb-3">
+                <div class="col-12 mt-5 mb-3 border-bottom border-top">
+                    <div class="article-share d-flex justify-content-center">
+                        <div class="article-share-bg a2a_kit a2a_kit_size_32 a2a_default_style">
+                            <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+                            <a class="a2a_button_facebook"></a>
+                            <a class="a2a_button_twitter"></a>
+                            <a class="a2a_button_email"></a>
+                        </div>
+                        <script async src="https://static.addtoany.com/menu/page.js"></script>
+                    </div>
                     <?php wpb_posts_nav(); ?>
                 </div>
             <?php if (comments_open() || get_comments_number()) :
