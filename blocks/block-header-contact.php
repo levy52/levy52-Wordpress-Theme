5ff90a12db30f54544a949c879bb49624b42629e
levy52 <radeklewicki52@gmail.com>
2022-07-24 11:00:42 +0200

    the menu has been styled

--- blocks/block-header-contact.php
+++ blocks/block-header-contact.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-12 contact-image">
         <picture>
-            <source media="(max-width: 767px)" srcset="<?php block_field('custom-header-mobile'); ?>" >
+            <source media="(max-width: 767px)" srcset="<?php block_field('custom-header-mobile'); ?>" alt="Contact image">
             <img class="img-fluid" src="<?php block_field('custom-header'); ?>" alt="Contact image">
         </picture>
         <span class="contact-text"><?php block_field('text'); ?></span>
