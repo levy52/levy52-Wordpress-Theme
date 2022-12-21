73a0d77aa2c74a5f4049d2a3ff4b19d241e87f7f
levy52 <radeklewicki52@gmail.com>
2022-12-21 16:59:25 +0100

    fixed CLS

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@ get_header();
 <?php get_template_part('/template-parts/home-slider-loop'); ?>
 </div>
 
-<div class="container mb-4 mt-5 text-color">
+<div class="container mb-4  text-color">
     <div class="row">
         <div class="col-12 col-md-8 col-xxl-9">
             <?php get_template_part('/template-parts/custom-loop'); ?>
